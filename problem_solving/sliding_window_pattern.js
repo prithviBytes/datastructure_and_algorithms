873251c5f7ad5bc04b80d4aed013d5edd3a96437
prithviBytes <prithvisuvarna56@gmail.com>
2023-02-03 12:08:04 +0530

    add: sliding window pattern

---
+++ problem_solving/sliding_window_pattern.js
@@ -0,0 +1,37 @@
+function maxSubArraySum_0n2(arr, num) {
+    if(arr.length < num){
+        return null;
+    }
+    let max = -Infinity;
+    for(let i = 0; i <= arr.length - num; i++){
+        let temp = 0
+        for(let j = 0; j < num; j++){
+            temp += arr[i + j];
+        }
+        if (temp > max){
+            max = temp;
+        }
+    }
+    return max;
+}
+
+
+function maxSubArraySum_0n(arr, num){
+    if (arr.length < num) {
+        return null;
+    }
+    let max = 0;
+    for(let i = 0; i < num; i++){
+        max += arr[i];
+    }
+    for(let i = num; i < arr.length; i++){
+        let temp = max + arr[i] - arr[i - num]
+        if(temp > max){
+            max = temp;
+        }
+    }
+    return max
+}
+
+console.log(maxSubArraySum_0n2([1,2,3,4,5,4,5,6,8,4], 3))
+console.log(maxSubArraySum_0n([1,2,3,4,5,4,5,6,8,4], 3))
