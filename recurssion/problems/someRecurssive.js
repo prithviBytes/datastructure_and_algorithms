8053b76c42e50f419d0543d206809d2cf0fbfb09
prithviBytes <prithvisuvarna56@gmail.com>
2023-02-05 14:23:06 +0530

    add: someRecurrsive problem

---
+++ recurssion/problems/someRecurssive.js
@@ -0,0 +1,20 @@
+
+function isOdd(num){
+    return num % 2 !== 0 ? true : false;
+}
+
+const isEven = (num) => num % 2 === 0 ? true : false;
+
+// Write a recursive function called someRecursive which accepts an array and a callback. 
+// The function returns true if a single value in the array returns true when passed to
+// the callback. Otherwise it returns false.
+function someRecurssive(arr, callbackFunction){
+    if (arr.length === 0) return false;
+    if(callbackFunction(arr[0])){
+        return true;
+    }else{
+        return false || someRecurssive(arr.slice(1), callbackFunction)
+    }
+}
+
+console.log(someRecurssive([2,2,2,2,2], isOdd))
