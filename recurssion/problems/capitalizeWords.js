31e1761a6b910f9f256be10b9b6302124d480f5d
prithviBytes <prithvisuvarna56@gmail.com>
2023-02-06 21:33:37 +0530

    add: solutions to recurssion problems

--- recurssion/problems/capitalizeWords.js
+++ recurssion/problems/capitalizeWords.js
@@ -13,5 +13,16 @@ function capitalizeWords(arr) {
     return output;
 }
 
+
+function capitalizeWords (array) {
+    if (array.length === 1) {
+      return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length-1)[0].toUpperCase());
+    return res;
+   
+}
+
 let words = ['i', 'am', 'learning', 'recursion'];
 capitalizeWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
